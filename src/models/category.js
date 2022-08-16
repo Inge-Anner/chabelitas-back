da9859489ee53dfcb2d4a1a5a23c81525456e6f1
Amohrs20 <amohrs@miumg.edu.gt>
2022-08-15 20:27:47 -0600

    initial commit for chabela backend

---
+++ src/models/category.js
@@ -0,0 +1,33 @@
+const { Model } = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The models/index file will call this method automatically.
+     */
+    static associate(models) {
+    }
+  }
+
+  Category.init({
+    categoryId: {
+      type: DataTypes.INTEGER,
+      field: 'id_Categoria',
+    },
+    statusId: {
+      type: DataTypes.INTEGER,
+      field: 'id_estado',
+    },
+    categoryName: {
+      type: DataTypes.STRING,
+      field: 'nombre_categoria',
+    },
+  }, {
+    sequelize,
+    modelName: 'Category',
+    timestamps: false,
+  });
+  return Category;
+}
